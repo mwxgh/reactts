2b633aa5b7cc466b37eee89cc8b5bfde52345fa9
mwxgh <mwx6996@gmail.com>
2022-11-07 18:49:56 +0700

    711

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import Meta from '../components/Meta'
 import Navbar from '../components/Navbar'
-import Slider from '../components/Slider'
 
 export default function Home() {
   return (
     <div>
       <Meta />
       <Navbar />
-      {/* <Slider /> */}
     </div>
   )
 }
